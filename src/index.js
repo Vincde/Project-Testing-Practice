69f6055ecd72f24e9bc4b9c9ed626b59555dee0d
Vincde <145802178+Vincde@users.noreply.github.com>
2024-07-15 14:27:28 +0200

    Add further tests to check whether the string has special characters as first letter, or no characters at all

--- src/index.js
+++ src/index.js
@@ -1,6 +1,14 @@
 function capitalize(stringToBeCapitalized) {
+  let i = 0;
+  let specialChars = /[`!@#$%^&*()_\-+=\[\]{};':"\\|,.<>\/?~ ]/;
+  while (specialChars.test(stringToBeCapitalized.charAt(i)) === true) {
+    i++;
+    if (i >= stringToBeCapitalized.length) {
+      return "No valid characters found";
+    }
+  }
   return (
-    stringToBeCapitalized.charAt(0).toUpperCase() +
+    stringToBeCapitalized.charAt(i).toUpperCase() +
     stringToBeCapitalized.slice(1)
   );
 }
--- src/index.test.js
+++ src/index.test.js
@@ -1,7 +1,26 @@
 import capitalize from "./index.js";
 
 test("first letter is a capitalized letter", () => {
-  let aa = capitalize("heidi").charAt(0);
-  let bb = capitalize("heidi").charAt(0).toUpperCase();
-  expect(aa).toBe(bb);
+  let charToControl = capitalize("teidi");
+  expect(charToControl.charAt(0)).toBe("T");
+});
+
+test("first letter is a capitalized letter ALREADY", () => {
+  let charToControl = capitalize("Reidi");
+  expect(charToControl.charAt(0)).toBe("R");
+});
+
+test("first letter is a special character", () => {
+  let charToControl = capitalize("#eidi");
+  expect(charToControl.charAt(0)).toBe("E");
+});
+
+test("first and third letter is a special character", () => {
+  let charToControl = capitalize("#e#di");
+  expect(charToControl.charAt(0)).toBe("E");
+});
+
+test("all letter are special characters", () => {
+  let charToControl = capitalize("####");
+  expect(charToControl).toBe("No valid characters found");
 });
